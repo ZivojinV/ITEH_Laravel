78ec8ca5c960f10381d35818804ec336dfe570c0
“ZivojinV” <zile14veljkovic@gmail.com>
2023-04-27 21:57:20 +0200

    Dodati seederi i migracije za bazu podataka

---
+++ database/migrations/2023_01_08_012406_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname')->unique();
+            $table->text('lastname')->nullable();
+            $table->string('parent');
+            $table->double('grade');
+            $table->string('balance');
+            $table->foreignId('university_id');
+            $table->foreignId('city_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,60 +30,53 @@ class DatabaseSeeder extends Seeder
 
         University::insert([
             [
-                "name" => "Epiphone",
-                "city" => "Sparta",
-                "CEO" => "Jim Rosenberg"
+                "name" => "FON",
+                "city" => "Belgrade",
+                "CEO" => "Dusan Savic"
             ],
             [
-                "name" => "LTD",
-                "city" => "Tokyo",
-                "CEO" => "Masanori Yamada"
+                "name" => "FTN",
+                "city" => "Novi Sad",
+                "CEO" => "Pera Peric"
             ]
         ]);
 
         City::insert([
             [
-                "name" => "Les Paul",
-                "population" => 6,
-                "is_capital" => true,
-                "history" => "right-handed",
+                "name" => "Pancevo",
+                "population" => 60000,
+                "is_capital" => false,
+                "history" => "Pancevo is city known for factories and beers.",
                 "GDP" => 24.75
             ],
             [
-                "name" => "SG",
-                "population" => 6,
-                "is_capital" => true,
-                "history" => "right-handed",
+                "name" => "Nis",
+                "population" => 72000,
+                "is_capital" => false,
+                "history" => "Nis has Cele Kula which is made of human skulls.",
                 "GDP" => 25.5
             ]
         ]);
 
 
         $student_1 = new Student;
-        $student_1->firstname = "Prophecy";
-        $student_1->parent = "Midnight Ebony Black";
-        $student_1->lastname = "This Les Paul Prophecy Custom EX Electric Student deploys an EMG-81 humbucker in the bridge position for great attack and sustain and an EMG-85 humbucker at the neck for fat, great-sounding rhythm. Lavish features on the Prophecy EX Les Paul student include a highly figured quilt maple top, pearl inlay knobs, strap locks, graphite nut, Grover tuners, a bound ebony balance, unique blade inlay, and a deep, rich midnight ebony finish. The mahogany set neck and 24-fret ebony balance provide a playground two full octaves in length. The LockTone Tune-O-Matic bridge and stopbar tailpiece add more sustain and make string changing easier.";
-        $student_1->grade = "86421";
-        $student_1->balance = "Ebony";
+        $student_1->firstname = "Zivojin";
+        $student_1->parent = "Miroslav";
+        $student_1->lastname = "Veljkovic";
+        $student_1->grade = "8";
+        $student_1->balance = "80000";
         $student_1->university_id = 1;
         $student_1->city_id = 1;
         $student_1->save();
 
         Student::create([
-            "firstname" => "VIPER-300FM",
-            "parent" => "Black",
-            "lastname" => "LTD Viper-300FM električna gitara ima fantastičan osećaj sa dva useka u telu i tankom U konturom vrata koja omogućava izvanredan pristup celom freatboard-u.  Super odsečene ivice doprinose modernom izgledu. Mahogany telo i flamed maple top, u kombinaciji sa EMG 81 (bridge) i EMG85 (neck) pickup-ima dobijate napadniji zvuk, savršen za rok. Skala od 24-3/4 incha, zajedno sa tankim vratom čini sviranje ove sekire prostim kao pasulj. Druge odlike su crni, niklovani hardver, 42mm standardni navrtanj, ESP čivije, tune-o-matic most i tailpiece, pickup selektor i volume i tone potenciometri.",
-            "grade" => "82084",
-            "balance" => "Rosewood",
+            "firstname" => "Milan",
+            "parent" => "Stefan",
+            "lastname" => "Markovic",
+            "grade" => "9",
+            "balance" => "90000",
             "university_id" => 2,
             "city_id" => 2
         ]);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
